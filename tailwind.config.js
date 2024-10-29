319fa0f40012d071e866121d40c8b9fb5dba97c5
Kilina_Irina <gisi@bk.ru>
2024-10-30 00:19:31 +0500

    Задание 6: подключила breeze и flowbite.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+      ],
+      theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+      },
+      plugins: [
+          require('flowbite/plugin'),
+          forms
+      ],
+};
