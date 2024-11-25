171e5861625e7ad877acd3dd30ae008974b7dc5a
Kilina_Irina <gisi@bk.ru>
2024-11-25 20:28:34 +0500

    Прочистила проект от неактуальных файлов.

--- resources/views/dummy.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="w-full text-center">
-    <p class="text-5xl">Page is under construction!</p>
-</div>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MainController;
 use App\Http\Controllers\ReportController;
 use App\Http\Middleware\Admin;
 
@@ -16,18 +15,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/', [MainController::class, 'showIndex'])->name('home');
-
-    Route::get('/array', [MainController::class, 'showArray'])->name('products');
-    
-    Route::get('/reports', [ReportController::class, 'index'])->name('report.index');
-    
-    Route::delete('/reports/{report}', [ReportController::class, 'destroy'])->name('reports.destroy');
-    
-    Route::post('/reports', [ReportController::class, 'add'])->name('reports.add');
-    
-    Route::get('reports/{report}', [ReportController::class, 'show'])->name('reports.show');
-    Route::put('reports/{report}', [ReportController::class, 'update'])->name('reports.update');
 });
 
 Route::middleware((Admin::class))->group(function() {
