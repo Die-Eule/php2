f9271bd617107d22f70dbb4a2ad2bc72ddace856
Kilina_Irina <gisi@bk.ru>
2024-12-16 02:34:29 +0500

    Сделала чётные-нечётные строки в таблице разных цветов.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,21 +9,46 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <x-nav-link :href="route('reports.new')">
-                        {{ __('создать заявление') }}
-                    </x-nav-link>
+                    <form action="{{ route('reports.new') }}" method="GET">
+                        <button class="p-6 bg-slate-200 rounded-md p-3 hover:bg-slate-400 transition" type="submit">создать заявление</button>
+                    </form>
                     @include('layouts.flash-messages')
                     <div class='w-full sm:p-5 md:p-10 lg:p-20'>
-                        <div class="grid grid-cols-1 lg:grid-cols-[6rem_minmax(0,1fr)_10rem_7rem] gap-4">
-                            <p class="font-bold hidden lg:block">Номер</p>
-                            <p class="font-bold hidden lg:block">Описание</p>
-                            <p class="font-bold hidden lg:block">Дата создания</p>
-                            <p class="font-bold hidden lg:block">Статус</p>
+                        <div class="grid grid-cols-1 lg:grid-cols-[6rem_minmax(0,1fr)_10rem_7rem]">
+                            <p class="font-bold hidden lg:block pr-4">Номер</p>
+                            <p class="font-bold hidden lg:block pr-4">Описание</p>
+                            <p class="font-bold hidden lg:block pr-4">Дата создания</p>
+                            <p class="font-bold hidden lg:block pr-4">Статус</p>
                             @foreach ($reports as $report)
-                                <p class="text-base underline lg:no-underline">{{$report['number']}}</p>
-                                <p class="text-base break-words bg-slate-100 lg:bg-inherit">{{$report['description']}}</p>
-                                <p class="text-base">{{$report['created_at']}}</p>
-                                <p class="text-base {{$colors[$report->status_id]}} border-gray-500 border-b-2 lg:border-0">{{$statuses->find($report->status_id)->name}}</p>
+                                <p @class([
+                                    'pr-4',
+                                    'text-base',
+                                    'underline',
+                                    'lg:no-underline',
+                                    'bg-gray-100' => $loop->odd
+                                    ])>{{$report['number']}}</p>
+                                <p @class([
+                                    'pr-4',
+                                    'text-base',
+                                    'break-words',
+                                    'bg-slate-100',
+                                    'lg:bg-inherit' => $loop->even,
+                                    'lg:bg-gray-100' => $loop->odd
+                                    ])>{{$report['description']}}</p>
+                                <p @class([
+                                    'pr-4',
+                                    'text-base',
+                                    'bg-gray-100' => $loop->odd
+                                    ])>{{$report['created_at']}}</p>
+                                <p @class([
+                                    'pr-4',
+                                    'text-base',
+                                    $colors[$report->status_id],
+                                    'border-gray-500',
+                                    'border-b-2',
+                                    'lg:border-0',
+                                    'bg-gray-100' => $loop->odd
+                                    ])">{{$statuses->find($report->status_id)->name}}</p>
                             @endforeach
                         </div>
                     </div>
